287c6eca69f4466db6f7cd0135fdcdce593c97ed
AlexDeusdete <alexdeusdete@hotmail.com>
2020-06-09 10:12:22 -0300

    Add basic icons

---
+++ Crochet/Interfaces/IBrand.cs
@@ -0,0 +1,13 @@
+﻿using Crochet.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crochet.Interfaces
+{
+    public interface IBrand
+    {
+        IList<Brand> GetItems();
+        Brand PutItem(Brand Item);
+    }
+}
---
+++ Crochet/Interfaces/IFeedStockService.cs
@@ -0,0 +1,13 @@
+﻿using Crochet.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crochet.Interfaces
+{
+    public interface IFeedStockService
+    {
+        IList<FeedStock> GetItems();
+        FeedStock PutItem(FeedStock Item);
+    }
+}
